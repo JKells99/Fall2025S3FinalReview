3af9fd27bd8b9a413f65288bda28faa17a2fa8af
Jordan Kelloway <106637327+JKells99@users.noreply.github.com>
2025-11-25 12:05:30 -0330

    add password hashing for new users in UserService

--- src/main/java/com/keyin/App.java
+++ src/main/java/com/keyin/App.java
@@ -10,8 +10,8 @@ public class App {
     public static void main(String[] args) throws SQLException {
         UserService userService = new UserService();
 
-        Admin admin = new Admin("admin1", "password123", "admin@school.com", "123-123-1233", 100000, true);
-//        userService.saveNewUser(admin);
+        Admin admin = new Admin("admin2", "password123", "admin2@school.com", "123-123-1233", 100000, true);
+        userService.saveNewUser(admin);
         User searchedUser = userService.getUserByUserName("admin1");
 
         System.out.println(searchedUser);
